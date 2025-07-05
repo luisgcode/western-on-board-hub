edc04dbd2a82d99121e1b1579930f1a6e96635dd
Luis Guaiquirian <luisgcode@gmail.com>
2025-07-05 14:00:42 -0400

    refactor(folders): Changing naming system to Pascalcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className="">Hello</h1>
-    </>
+    </div>
   );
 }
 
