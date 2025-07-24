4b6c0c33da26db6fcfd80d70f8dde3cff5feff7f
Luis G <luisgcode@gmail.com>
2025-07-24 06:51:49 -0400

    refactor(folders): Creating new fodlers and organizing better

--- src/components/Documents/DocumentsPage.jsx
+++ src/components/Documents/DocumentsPage.jsx
@@ -5,17 +5,16 @@ const DocumentsPage = () => {
   return (
     <>
       <div className="flex gap-10 flex-wrap">
-        {companyDocuments.map(function (item) {
-          return (
-            <DocumentCard
-              name={item.name}
-              id={item.id}
-              description={item.description}
-              btn={item.filePath}
-              category={item.category}
-            />
-          );
-        })}
+        {companyDocuments.map((item) => (
+          <DocumentCard
+            key={item.id}
+            name={item.name}
+            id={item.id}
+            description={item.description}
+            btn={item.filePath}
+            category={item.category}
+          />
+        ))}
       </div>
     </>
   );
